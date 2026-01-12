192fb1719db1611b168e9eee86899c7d06f0c97d
NodEmKay <mkaydevops@gmail.com>
2026-01-12 19:30:31 +0000

    Remove nested Git repository

---
+++ Day-2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "Ubuntu AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+}
+
+
