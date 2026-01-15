599b1d59ef855ca89b3f57c6af6bd6637050656e
NodEmKay <mkaydevops@gmail.com>
2026-01-15 23:36:29 +0000

    update

--- Day-3/project/dev/main.tf
+++ Day-3/project/dev/main.tf
@@ -2,12 +2,33 @@ provider "aws" {
   region = var.aws_region
 }
 
+# EC2 Instances
 resource "aws_instance" "server" {
   count         = length(var.instance_names) # Better than hardcoding '3'
   ami           = var.ami
   instance_type = var.instance_type
+  # key_name = "dummy"
 
   tags = {
+    #Name = "SRV105"
     Name = var.instance_names[count.index]
   }
 }
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.s3_bucket_name
+
+  tags = {
+    Name = var.s3_bucket_name
+  }
+
+}
+  
+# ONLY Versioning Resource
+#resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+ # bucket = aws_s3_bucket.my_bucket.id
+  
+  #versioning_configuration {
+   # status = "Enabled"
+  #}
+#}
--- Day-3/project/dev/variables.tf
+++ Day-3/project/dev/variables.tf
@@ -5,3 +5,6 @@ variable "instance_names" {
     type    = list(string)
     default = ["srv101", "srv102", "srv103"]
  }
+variable "s3_bucket_name" {
+    type = string
+}
