68b8b46fb55406c7aa16278fcf23372d1add1a32
NodEmKay <mkaydevops@gmail.com>
2026-01-17 20:16:47 +0000

    udpated1

--- Day-4/remote-state/backend.tf
+++ Day-4/remote-state/backend.tf
@@ -3,6 +3,9 @@ terraform {
     bucket = "node-s3-bucket0011"
     key = "terraform.state"
     region = "us-east-1"
+        # Enable S3 native locking
+    use_lockfile = true
+    # The dynamo DB arguement is no longer needed
 }
 
 }
