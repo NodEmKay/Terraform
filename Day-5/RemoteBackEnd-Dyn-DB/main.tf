b9b1f2443b8c3d861f720c9a905843bcc7925892
NodEmKay <mkaydevops@gmail.com>
2026-01-18 11:26:09 +0000

    update

--- Day-5/RemoteBackEnd-Dyn-DB/main.tf
+++ Day-5/RemoteBackEnd-Dyn-DB/main.tf
@@ -54,3 +54,13 @@ tags = {
  Name = "${var.instance_name}-5"
 }
 }
+
+resource "aws_instance" "aws_linux-6" {
+ami           = var.ami
+instance_type = var.instance_type
+key_name      = aws_key_pair.aws_linux.key_name
+
+tags = {
+ Name = "${var.instance_name}-6"
+}
+}
