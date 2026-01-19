0adf88d2ac2f36c84785547eed5d87371dfd2507
NodEmKay <mkaydevops@gmail.com>
2026-01-19 20:51:36 +0000

    udpate

--- Day-7/watch_ec2_state_take_snap_shot/main.tf
+++ Day-7/watch_ec2_state_take_snap_shot/main.tf
@@ -92,4 +92,20 @@ resource "aws_cloudwatch_event_rule" "ec2_shutdown_rule" {
   name = "watch-ec2-shutting-down"
   event_pattern = jsonencode({
     "source": ["aws.ec2"],
-    "detail-
+    "detail-type": ["EC2 Instance State-change Notification"],
+    "detail": { "state": ["shutting-down"] }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.ec2_shutdown_rule.name
+  target_id = "TriggerLambda"
+  arn       = aws_lambda_function.snapshot_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.snapshot_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ec2_shutdown_rule.arn
+}
