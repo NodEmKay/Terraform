3a0b7f66e88b5d7dd70a7cc06089fe288fe913fa
NodEmKay <mkaydevops@gmail.com>
2026-01-31 08:34:13 +0000

    update

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 resource "aws_instance" "example" {
   count         = 2
   ami           = "ami-0532be01f26a3de55" # Amazon Linux 2 AMI (update based on region)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Dev-Server0001"
