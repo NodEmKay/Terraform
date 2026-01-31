0eddbc49f37c36254c74019ad37828b0bd1a9f04
NodEmKay <mkaydevops@gmail.com>
2026-01-31 09:00:56 +0000

    update

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "NodeEmkay"
+terraform { 
+  cloud { 
+    
+    organization = "NodeEmkay" 
 
-    workspaces {
-      name = "Terraform"
-    }
-  }
+    workspaces { 
+      name = "Terraform1" 
+    } 
+  } 
 }
 
 resource "aws_instance" "example" {
   count         = 2
-  ami           = "ami-0b6c6ebed2801a5cb" # Amazon Linux 2 AMI (update based on region)
-  instance_type = "t2.medium"
+  ami           = "ami-0532be01f26a3de55" # Amazon Linux 2 AMI (update based on region)
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Dev-Server0001"
