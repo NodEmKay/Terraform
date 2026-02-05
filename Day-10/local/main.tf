3dd6739a5af7f590126971fa2a1eadfad449cb84
NodEmKay <mkaydevops@gmail.com>
2026-02-05 19:48:56 +0000

    push

--- Day-10/local/main.tf
+++ Day-10/local/main.tf
@@ -1,12 +1,50 @@
 locals {
-  region = "us-east-1"
-  instance_type = "t2.micro"
-  ami = "ami-0532be01f26a3de55"
-}
-resource "aws_instance" "local-pc1" {
-    ami           = local.ami
-    instance_type = local.instance_type
+  dev = {
+    environment   = "dev"
+    region        = "us-east-1"
+    instance_type = "t2.micro"
+    ami           = "ami-0532be01f26a3de55"
     tags = {
-      Name = "local-pc1"
+      Name        = "dev-pc1"
+      Environment = "dev"
     }
+  }
+  prod = {
+    environment   = "prod"
+    region        = "us-east-1"
+    instance_type = "t2.micro"
+    ami           = "ami-0532be01f26a3de55"
+    tags = {
+      Name        = "prod-pc1"
+      Environment = "prod"
+    }
+  }
+  local = {
+    environment   = "local"
+    region        = "us-east-1"
+    instance_type = "t2.micro"
+    ami           = "ami-0532be01f26a3de55"
+    tags = {
+      Name        = "local-pc1"
+      Environment = "local"
+    }
+  }
+}
+
+resource "aws_instance" "local-pc1" {
+  ami           = local.local.ami
+  instance_type = local.local.instance_type
+  tags          = local.local.tags
+}
+
+resource "aws_instance" "dev-pc1" {
+  ami           = local.dev.ami
+  instance_type = local.dev.instance_type
+  tags          = local.dev.tags
+}
+
+resource "aws_instance" "prod-pc1" {
+  ami           = local.prod.ami
+  instance_type = local.prod.instance_type
+  tags          = local.prod.tags
 }
