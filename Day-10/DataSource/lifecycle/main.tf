9150b9eaed252ac4ffd95ce7983809f2c26358cc
NodEmKay <mkaydevops@gmail.com>
2026-02-05 17:17:35 +0000

    update

--- Day-10/DataSource/lifecycle/main.tf
+++ Day-10/DataSource/lifecycle/main.tf
@@ -16,8 +16,8 @@ resource "aws_instance" "two" {
 
 resource "aws_s3_bucket" "one_s3_bucket" {
   bucket = "one-s3-bucket-01111"
-}
-
-lifecycle {
-  prevent_destroy = true
+  lifecycle {
+    #prevent_destroy = true
+    ignore_changes = [ tags ]
+  }
 }
